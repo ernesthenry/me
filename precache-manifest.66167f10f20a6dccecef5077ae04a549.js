52f0dddf741b8ae4c2dd48871333fff8041727da
ernesthenry <henry38ernest@gmail.com>
2020-06-11 16:06:04 +0300

    Updates

---
+++ precache-manifest.66167f10f20a6dccecef5077ae04a549.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89c167924bd77c6fe5405f32c0d3cf55",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "ac23c5e038bec5258d92",
+    "url": "/me/static/css/main.76ff40f4.chunk.css"
+  },
+  {
+    "revision": "1254522c79a5a73fe12f",
+    "url": "/me/static/js/2.2006df43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/me/static/js/2.2006df43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac23c5e038bec5258d92",
+    "url": "/me/static/js/main.c4d61e97.chunk.js"
+  },
+  {
+    "revision": "117b1daf9eef0a5ed5a6",
+    "url": "/me/static/js/runtime-main.833d48b3.js"
+  }
+]);
