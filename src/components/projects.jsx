73bb3813c0fbb8519134bb14faae659acf07e83e
ernesthenry <henry38ernest@gmail.com>
2020-06-14 14:22:55 +0300

    add  external linkks

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -17,8 +17,8 @@ export default class Projects extends Component {
 								<div className="project" style={{backgroundImage: 'url(images/img-1.jpg)'}}>
 									<div className="desc">
 										<div className="con">
-											<h3><a href="work.html">Work 01</a></h3>
-											<span>Website</span>
+											<h3><a href="https://ernesthenry.github.io/books-app/"  target="_blank" rel="noopener noreferrer">Bookshelf MyReads</a></h3>
+											<span>● Built a bookshelf app that allows you to select and categorize books you have read, are currently reading, or want to read. </span>
 										
 										</div>
 									</div>
@@ -29,9 +29,9 @@ export default class Projects extends Component {
 									<div className="desc">
 										<div className="con">
 											<a href='#projects'>
-											<h3><a href="work.html">Work 02</a></h3>
-											<span>Animation</span>
-											<span>blog app </span>
+											<h3><a href="https://ernesthenry.github.io/React_Working_With_APIS/" target="_blank" rel="noopener noreferrer">React News App</a></h3>
+											<span>News app that consumes a news API to provides latest news from popular sites across the world</span>
+											
 
 											</a>
 											
@@ -56,8 +56,8 @@ export default class Projects extends Component {
 								<div className="project" style={{backgroundImage: 'url(images/img-4.jpg)'}}>
 									<div className="desc">
 										<div className="con">
-											<h3><a href="work.html">Work 04</a></h3>
-											<span>Application</span>
+											<h3><a href=" https://ernesthenry.github.io/reactinterface/ "  target="_blank" rel="noopener noreferrer">Pet medicine appointment app</a></h3>
+											<span>Single page, responsive frontend  app for making appointments for pet medicine</span>
 										
 										</div>
 									</div>
@@ -67,8 +67,8 @@ export default class Projects extends Component {
 								<div className="project" style={{backgroundImage: 'url(images/img-5.jpg)'}}>
 									<div className="desc">
 										<div className="con">
-											<h3><a href="work.html">Work 05</a></h3>
-											<span>Graphic, Logo</span>
+											<h3><a href="https://ernesthenry.github.io/me" target="_blank" rel="noopener noreferrer">Personal portfolio</a></h3>
+											<span>Responsive, single page application for my portfolio</span>
 										
 										</div>
 									</div>
@@ -78,8 +78,8 @@ export default class Projects extends Component {
 								<div className="project" style={{backgroundImage: 'url(images/img-6.jpg)'}}>
 									<div className="desc">
 										<div className="con">
-											<h3><a href="work.html">Work 06</a></h3>
-											<span>Web Design</span>
+											<h3><a href="https://www.prowebin.co.ug/" target="_blank" rel="noopener noreferrer">Prowebin company website</a></h3>
+											<span>Digital solutions  company</span>
 										
 										</div>
 									</div>
