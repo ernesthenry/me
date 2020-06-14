2abd247c27313da33e19776f45c7b1b7d06d3b60
ernesthenry <henry38ernest@gmail.com>
2020-06-14 16:01:40 +0300

    add links

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -17,7 +17,7 @@ export default class Projects extends Component {
 								<div className="project" style={{backgroundImage: 'url(images/img-1.jpg)'}}>
 									<div className="desc">
 										<div className="con">
-											<h3><a href="https://ernesthenry.github.io/books-app/"  target="_blank" rel="noopener noreferrer">Bookshelf MyReads</a></h3>
+											<h3><a href="https://github.com/ernesthenry/books-app/"  target="_blank" rel="noopener noreferrer">Bookshelf MyReads</a></h3>
 											<span>● Built a bookshelf app that allows you to select and categorize books you have read, are currently reading, or want to read. </span>
 										
 										</div>
